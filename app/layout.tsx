bf2bbd0bb916e947038080ec0d04c2286df8fe0a
Yann-Donald <yanndonald237@gmail.com>
2024-12-04 01:03:40 +0100

    assets and theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const FontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -27,7 +28,9 @@ export default function RootLayout({
           FontSans.variable
         )}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex h-screen ">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="mx-auto"
+          />
+        </div>
+      </section>
     </div>
   );
 }
