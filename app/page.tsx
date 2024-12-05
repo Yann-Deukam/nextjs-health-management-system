ee4515de0229cc4ece2979be59cf250e4fceaf57
Yann-Donald <yanndonald237@gmail.com>
2024-12-05 03:11:30 +0100

    resgistration form

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,39 @@
+import { PatientForm } from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex h-screen ">
+      {/* /* TODO: OTP FORM */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
-            src="/logo.svg"
+            src="/assets/icons/logo-full.svg"
             alt="logo"
-            width={0}
-            height={0}
-            sizes="100vw"
-            className="mx-auto"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
           />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              &copy; 2024, All rights reserved
+            </p>
+            <Link href="/?admin=true" className="text-blue-500">
+              Admin Panel
+            </Link>
+          </div>
         </div>
       </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="onboarding"
+        className="side-img max-w-[50%] object-cover"
+      />
     </div>
   );
 }
